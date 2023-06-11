62828dce9cc110fd0249fb679b0f0a96cccdc5cb
Tao Tan <126834441+ttan0091@users.noreply.github.com>
2023-06-11 16:00:09 +1000

    navbar modification

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,29 @@
 import React from "react";
 import "./navbar.css";
+import logo from "../../assets/logo.svg";
+import{RiMenu3Line, RiCloseLine} from "react-icons/ri";
+
 const Navbar = () => {
     return (
-        <div>
-            <p>Navbar</p>
+        <div className="gpt3__navbar">
+            <div className="gpt3__navbar-links">
+                <div className="gpt3__navbar-links_logo">
+                    <img src={logo} alt="logo"/>
+                </div>
+                <div className="gpt3__navbar-links_container">
+                    <p><a href="home"></a>Home</p>
+                    <p><a href="wgpt3"></a>What is GPT3?</p>
+                    <p><a href="possibility"></a>Open AI</p>
+                    <p><a href="features"></a>Case Studies</p>
+                    <p><a href="blog"></a>Library</p>
+                </div>
+                <div className="gpt3__navbar-sign">
+                    <p>Sign in</p>
+                    <button type="button">Sign in</button>
+
+
+                </div>
+            </div>
         </div>
     );
 }
