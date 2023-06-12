a52f3def6c4074e88904acd66d68f1edffa91b0b
Tao Tan <126834441+ttan0091@users.noreply.github.com>
2023-06-12 19:46:35 +1000

    add brand

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,9 +1,27 @@
 import React from "react";
 import "./header.css";
+import people from "../../assets/people.png";
+import ai from "../../assets/ai.png";
 const Header = () => {
     return (
-        <div>
-            <p>Header</p>
+        <div className="gpt3__header section__padding" id="home">
+            <div className="gpt3__header-content">
+                <h1 className="gradient__text">
+                    Let’s Build Something amazing with GPT-4 OpenAI
+                </h1>
+                <p>Yet bed any for travelling assistance indulgence unpleasingNot thoughts all exercise blessing.Indulgence wayeverything joy alteration boisterous the attachment. Partywe years to order allow asked of.</p>
+                <div className="gpt3__header-content__input">
+                    <input type="email" placeholder="Your email address" />
+                    <button type="button">Get Started</button>
+                </div>
+                <div className="gpt3__header-content__people">
+                    <img src={people} alt="people"/>
+                    <p>1,600 people requested access a visit in last 24 hours</p>
+                </div>
+            </div>
+            <div className="gpt3__header-image">
+                <img src={ai} alt="ai"/>
+            </div>
         </div>
     );
 }
--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import "./whatGPT3.css";
+import {Feature} from "../../components";
+
 const WhatGPT3 = () => {
     return (
-        <div>
-            <p>WhatGPT3</p>
+        <div className="gpt3__whatgpt3 section__margin" id = "whpt3">
+            <div className="gpt3__whatgpt3-feature">
+                <Feature />
+            </div>
+            <div className="gpt3__whatgpt3-heading">
+                <h1 className="gradient__text">The possibility is beyond your imagination</h1>
+                <p>Explore The Library</p>
+            </div>
         </div>
     );
 }
