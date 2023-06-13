cba1166708fda6165322c2c6e33509083404ca4d
Tao Tan <126834441+ttan0091@users.noreply.github.com>
2023-06-13 23:43:44 +1000

    finish all the code

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -2,8 +2,15 @@ import React from "react";
 import "./cta.css";
 const CTA = () => {
     return (
-        <div>
-            <p>CTA</p>
+        <div className="gpt3__cta">
+            <div className="gpt3__cta-content">
+                <p>Request Early Access to Get Started</p>
+                <h3>Register today & start exploring the endless possiblities.</h3>
+            </div>
+            <div className="gpt3__cta-button">
+                <button type="button">Get Started</button>
+
+            </div>
         </div>
     );
 }
--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -2,8 +2,45 @@ import React from "react";
 import "./footer.css";
 const Footer = () => {
     return (
-        <div>
-            <p>Footer</p>
+        <div className="gpt3__footer section__padding">
+            <div className="gpt3__footer-heading">
+                <h1 className="gradient__text">Do you want to step in to the future before others</h1>
+            </div>
+
+            <div className="gpt3__footer-btn">
+                <p>Request Early Access</p>
+            </div>
+
+            <div className="gpt3__footer-links">
+                <div className="gpt3__footer-links-logo">
+                    <h4>GPT-4</h4>
+                    <p>Powerful conversational AI model for interactive communication.</p>
+                </div>
+                <div className="gpt3__footer-links_div">
+                    <h4>Links</h4>
+                    <p>Overwrite</p>
+                    <p>Social Media</p>
+                    <p>Counters</p>
+                    <p>Contact</p>
+                </div>
+                <div className="gpt3__footer-links_div">
+                    <h4>Company</h4>
+                    <p>Terms & Conditions</p>
+                    <p>Privacy Policy</p>
+                    <p>Contact</p>
+                </div>
+                <div className="gpt3__footer-links_div">
+                    <h4>Get in touch</h4>
+                    <p>Overwrite</p>
+                    <p>085-1247543</p>
+                    <p>Counters</p>
+                </div>
+            </div>
+
+            <div className="gpt3__footer-copyright">
+                <p>© 2023 GPT-4. All rights reserved.</p>
+            </div>
+
         </div>
     );
 }
